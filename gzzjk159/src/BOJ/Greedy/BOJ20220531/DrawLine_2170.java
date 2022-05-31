8f7f0ee845cf2d3a6a02936a22572fb33e8f87fa
gzzjk159 <gzzjk159@gmail.com>
2022-06-01 03:39:06 +0900

    2022-05-31

---
+++ gzzjk159/src/BOJ/Greedy/BOJ20220531/DrawLine_2170.java
@@ -0,0 +1,50 @@
+package BOJ.Greedy.BOJ20220531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class DrawLine_2170 {
+    public static void main(String[] args) throws NumberFormatException,IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] pos = new int[n][2];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            pos[i][0] = x;
+            pos[i][1] = y;
+        }
+
+        Arrays.sort(pos , new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                if (o1[0]==o2[0]) return o1[1]-o2[1];
+                else return o1[0]-o2[0];
+            }
+        });
+        int min = pos[0][0];
+        int max = pos[0][1];
+        answer = max - min;
+        for (int i = 1; i < n; i++) {
+            if (min <= pos[i][0] && pos[i][1]<=max) {
+                continue;
+            }
+            else if (pos[i][0]< max) {
+                answer += pos[i][1] - max;
+            }
+            else {
+                answer += pos[i][1] - pos[i][0];
+            }
+            min = pos[i][0];
+            max = pos[i][1];
+        }
+        System.out.println(answer);
+    }
+}
---
+++ gzzjk159/src/BOJ/Greedy/BOJ20220531/LineUP_7570.java
@@ -0,0 +1,25 @@
+package BOJ.Greedy.BOJ20220531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class LineUP_7570 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] line = new int[n+1];
+        int max = 0;
+
+        String[] child = br.readLine().split(" ");
+
+        for (int i = 0; i < n ; i++) {
+            int k = Integer.parseInt(child[i]);
+            line[k] = line[k-1] +1;
+            max = Math.max(line[k], line[k-1]+1);
+        }
+
+        System.out.println(n-max);
+    }
+}
